5f97edff8078191f78eaf14ed789edc77e9d7cb4
Cyrill Augustiny <c.augustiny@gmail.com>
2024-07-05 11:34:34 +0200

    Edit button
    Changed Script so that button is shown as "Bearbeiten" when user is admin

--- OnlineShop/scripts/script.js
+++ OnlineShop/scripts/script.js
@@ -59,7 +59,7 @@ document.addEventListener('DOMContentLoaded', async () => {
                                         <h5 class="card-title">${product.name}</h5>
                                         <p class="card-text">${product.price}.-</p>
                                         <p class="card-text">Kategorie: ${category.name}</p>
-                                        <a href="/products/edit/${product.id}" class="btn btn-primary stretched-link">Ansehen</a>
+                                        <a href="/products/edit/${product.id}" class="btn btn-primary stretched-link">Bearbeiten</a>
                                     </div>
                                 </div>    
                             `;
